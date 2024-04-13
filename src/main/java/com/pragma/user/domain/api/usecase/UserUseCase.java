a42bf3732186a040209a79ade8aaa3985a5842c8
Victor Agudelo <viactur11@gmail.com>
2024-04-13 14:07:38 -0500

    Se agregaron las dependencias de Spring Security, y JsonWebToken

--- src/main/java/com/pragma/user/domain/api/usecase/UserUseCase.java
+++ src/main/java/com/pragma/user/domain/api/usecase/UserUseCase.java
@@ -7,6 +7,7 @@ import com.pragma.user.domain.models.User;
 import com.pragma.user.domain.spi.IRolPersistencePort;
 import com.pragma.user.domain.spi.IUserPersistencePort;
 import com.pragma.user.domain.util.DomainConstants;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Objects;
 import java.util.Optional;
@@ -15,16 +16,22 @@ public class UserUseCase implements IUserServicePort {
 
   private final IUserPersistencePort userPersistencePort;
   private final IRolPersistencePort rolPersistencePort;
+  private final PasswordEncoder passwordEncoder;
 
-  public UserUseCase(IRolPersistencePort rolPersistencePort, IUserPersistencePort userPersistencePort) {
+  public UserUseCase(IRolPersistencePort rolPersistencePort,
+                     IUserPersistencePort userPersistencePort,
+                     PasswordEncoder passwordEncoder) {
+
     this.rolPersistencePort = rolPersistencePort;
     this.userPersistencePort = userPersistencePort;
+	  this.passwordEncoder = passwordEncoder;
   }
 
   @Override
   public void register(User user) {
     executeVerifyExistUser(user.getEmail());
     executeAdminCreation(user);
+    encoderPassword(user);
     userPersistencePort.saveUser(user);
   }
 
@@ -57,6 +64,10 @@ public class UserUseCase implements IUserServicePort {
                  DomainConstants.NOT_FOUND_MESSAGE,
                  DomainConstants.Class.ROLE.name()))
        );
+  }
+
+  private void encoderPassword(User user) {
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
   }
 
 }
