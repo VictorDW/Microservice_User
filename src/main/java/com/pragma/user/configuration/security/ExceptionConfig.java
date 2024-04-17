f4e8514551e60103bf90408b4e2f4720002eaa7c
Victor Agudelo <viactur11@gmail.com>
2024-04-17 18:09:24 -0500

    Se agrego la implementación personalizada del AccessDeniedHandler para el manejo de la excepción cuando se esta autenticado pero no autorizado

--- src/main/java/com/pragma/user/configuration/security/ExceptionConfig.java
+++ src/main/java/com/pragma/user/configuration/security/ExceptionConfig.java
@@ -2,6 +2,7 @@ package com.pragma.user.configuration.security;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pragma.user.configuration.Constants;
 import com.pragma.user.configuration.error.ExceptionManager;
 import com.pragma.user.configuration.error.dto.ExceptionResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +12,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @RequiredArgsConstructor
 public class ExceptionConfig {
 
   private final Logger loggerClass = LoggerFactory.getLogger(ExceptionConfig.class);
+  private static final String CONTENT_TYPE = "application/json";
+  private static final String CHARACTER_ENCODING = "UTF-8";
 
   @Bean
   public AuthenticationEntryPoint jwtAuthenticationEntryPoint() {
@@ -27,23 +31,34 @@ public class ExceptionConfig {
       if (messageToken != null) {
 
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(this.convertExceptionToString(messageToken.toString()));
+        response.setContentType(CONTENT_TYPE);
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        response.getWriter().write(this.convertExceptionToString(messageToken.toString(), HttpStatus.UNAUTHORIZED));
       }
     };
   }
 
-  private String convertExceptionToString(String message) {
+  @Bean
+  public AccessDeniedHandler permissionsAccessDeniedHandler() {
+    return (request, response, accessDeniedException) -> {
 
-    ExceptionResponse responseBody = ExceptionManager.generalExceptionHandler(message, HttpStatus.UNAUTHORIZED).getBody();
+      response.setStatus(HttpStatus.FORBIDDEN.value());
+      response.setContentType(CONTENT_TYPE);
+      response.setCharacterEncoding(CHARACTER_ENCODING);
+      response.getWriter().write(this.convertExceptionToString(accessDeniedException.getMessage(), HttpStatus.FORBIDDEN));
+    };
+  }
+
+  private String convertExceptionToString(String message, HttpStatus status) {
+
+    ExceptionResponse responseBody = ExceptionManager.generalExceptionHandler(message,status).getBody();
     ObjectMapper mapper = new ObjectMapper();
 
     try {
       return mapper.writeValueAsString(responseBody);
     } catch (JsonProcessingException e) {
       loggerClass.error(e.getMessage());
-      return "";
+      return Constants.ACCESS_DENIED_MESSAGE;
     }
   }
 }
