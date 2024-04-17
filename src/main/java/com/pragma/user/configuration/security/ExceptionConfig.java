b66b07887e548be5ef01936a47f56b51957ed49e
Victor Agudelo <viactur11@gmail.com>
2024-04-17 17:18:43 -0500

    Se movio de paquete la clase ExceptionConfig

--- src/main/java/com/pragma/user/configuration/security/ExceptionConfig.java
+++ src/main/java/com/pragma/user/configuration/security/ExceptionConfig.java
@@ -1,4 +1,4 @@
-package com.pragma.user.configuration;
+package com.pragma.user.configuration.security;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
