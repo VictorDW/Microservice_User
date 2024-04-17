c35a9d5b608822a92d3e63edbfe17cdb48b3cc34
Victor Agudelo <viactur11@gmail.com>
2024-04-17 18:10:49 -0500

    Se agrego la implementación del AccessDeniedHandler a la configuración de filtros

--- src/main/java/com/pragma/user/configuration/security/SecurityConfig.java
+++ src/main/java/com/pragma/user/configuration/security/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -32,6 +33,7 @@ public class SecurityConfig {
 
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final AuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final AccessDeniedHandler permissionsAccessDeniedHandler;
 	private final IUserRepository userRepository;
 
 	@Bean
@@ -42,7 +44,9 @@ public class SecurityConfig {
 				.sessionManagement(sessionConfig ->
 						sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authenticationProvider(authenticationProvider())
-				.exceptionHandling(exceptionConfig -> exceptionConfig.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+				.exceptionHandling(exceptionConfig -> exceptionConfig
+						.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+						.accessDeniedHandler(permissionsAccessDeniedHandler))
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
